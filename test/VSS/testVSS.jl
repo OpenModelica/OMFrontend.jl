60338133b858f50465302761cbaf5dbba146147a
JKRT <jtinnerholm@gmail.com>
2026-06-17 10:18:23 +0200

    Minor performance improvements

--- test/VSS/testVSS.jl
+++ test/VSS/testVSS.jl
@@ -1,4 +1,4 @@
-@testset "Structural transistions" begin
+@testset "Structural transitions" begin
   @test typeof(OMFrontend.parseFile("./VSS/BreakingPendulum.mo")) == Absyn.PROGRAM
   @test typeof(OMFrontend.parseFile("./VSS/SimpleSingleMode.mo")) == Absyn.PROGRAM
   res = flattenFM("SimpleSingleMode", "./VSS/SimpleSingleMode.mo")
